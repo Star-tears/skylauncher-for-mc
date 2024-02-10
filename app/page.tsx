fbca85f09f762b47f7512dc2f48f7ea95572663f
Star-tears <1370443225@qq.com>
2024-02-10 21:47:44 +0800

    add command-menu

--- app/page.tsx
+++ app/page.tsx
@@ -3,52 +3,57 @@
 // app/page.tsx
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { TitleBar } from "@/components/titlebar";
+import { TitleBar } from "@/components/skylauncher/titlebar";
 import { Avatar } from "@nextui-org/avatar";
 import { Toaster } from "@/components/ui/sonner";
 import useUpdater from "@/hooks/tauri/useUpdater";
-import { useTauriContext } from "@/components/providers/tauri-provider";
-import { CustomContextMenu } from "@/components/custom-context-menu";
+import { useTauriContext } from "@/components/skylauncher/providers/tauri-provider";
+import { CustomContextMenu } from "@/components/skylauncher/custom-context-menu";
+import { CommandMenu } from "@/components/skylauncher/command-menu";
 
 export default function Page() {
   const { t } = useTranslation();
   const { appVersion } = useTauriContext();
   useUpdater();
   return (
-    <CustomContextMenu>
-      <div className="flex h-screen w-screen flex-col">
-        <Toaster
-          closeButton
-          richColors
-          position="top-right"
-          className=" mt-10"
-          toastOptions={{
-            style: {
-              top: 24,
-            },
-          }}
-        />
-        <TitleBar />
-        <div className="flex h-full w-full flex-row">
-          <div className="flex h-full w-16 flex-col border-r-2">
-            <div>
-              <p className="select-none align-middle text-xl font-bold text-inherit ">
-                SkyLauncher
-              </p>
-            </div>
+    <>
+      <TitleBar />
+      <CommandMenu />
+      <CustomContextMenu>
+        <div className="flex h-screen w-screen flex-col">
+          <Toaster
+            closeButton
+            richColors
+            position="top-right"
+            className=" mt-10"
+            toastOptions={{
+              style: {
+                top: 24,
+              },
+            }}
+          />
 
-            <div className="mt-5 flex flex-col  items-center">
-              <Avatar isBordered color="secondary" radius="sm" name="Sky" />
+          <div className="flex h-full w-full flex-row">
+            <div className="flex h-full w-16 flex-col border-r-2">
+              <div>
+                <p className="select-none align-middle text-xl font-bold text-inherit ">
+                  SkyLauncher
+                </p>
+              </div>
+
+              <div className="mt-5 flex flex-col  items-center">
+                <Avatar isBordered color="secondary" radius="sm" name="Sky" />
+              </div>
             </div>
+            <div></div>
           </div>
-          <div></div>
+          <div className="fixed bottom-0 right-0 flex items-end justify-end p-1">
+            <span className="text-xs text-gray-500 opacity-50">
+              skylauncher-for-mc {t("version")} v{appVersion}
+            </span>
+          </div>
         </div>
-        <div className="fixed bottom-0 right-0 flex items-end justify-end p-1">
-          <span className="text-xs text-gray-500 opacity-50">
-            skylauncher-for-mc {t("version")} v{appVersion}
-          </span>
-        </div>
-      </div>
-    </CustomContextMenu>
+      </CustomContextMenu>
+    </>
   );
 }
--- app/providers.tsx
+++ app/providers.tsx
@@ -2,10 +2,10 @@
 "use client";
 
 import { NextUIProvider } from "@nextui-org/react";
-import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ThemeProvider } from "@/components/skylauncher/providers/theme-provider";
 import { useRouter } from "next/navigation";
 import "@/translations/i18n";
-import { TauriProvider } from "@/components/providers/tauri-provider";
+import { TauriProvider } from "@/components/skylauncher/providers/tauri-provider";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const router = useRouter();
