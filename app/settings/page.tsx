af0f38f7bd0c540be245f0aa3f0977cba317cbf8
Star-tears <1370443225@qq.com>
2024-02-23 13:34:08 +0800

    change ui style new-york

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -2,11 +2,13 @@
 
 import React from "react";
 import { BgImgSettings } from "@/components/skylauncher/unit/settings";
+import LangSettings from "@/components/skylauncher/unit/settings/lang-settings";
 
 export default function SettingsPage() {
   return (
-    <div className="mt-4 flex flex-col items-center">
+    <div className="mt-4 flex flex-col items-center gap-4">
       <BgImgSettings />
+      <LangSettings />
     </div>
   );
 }
---
+++ components/skylauncher/unit/settings/lang-settings.tsx
@@ -0,0 +1,52 @@
+import React, { Fragment } from "react";
+import {
+  SettingsBody,
+  SettingsHeader,
+  SettingsItem,
+} from "../../base/settings";
+import { useTranslation } from "react-i18next";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+function LangSettings() {
+  const { t,i18n } = useTranslation();
+  const languages = Object.keys(i18n.options.resources!);
+  const lang = i18n.resolvedLanguage;
+  return (
+    <SettingsItem>
+      <SettingsHeader>{t("Language Settings")}</SettingsHeader>
+      <SettingsBody>
+        <Select
+          defaultValue={lang}
+          onValueChange={(v) => {
+            console.log(v);
+            i18n.changeLanguage(v);
+          }}
+        >
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Select a Language" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>{t("Languages")}</SelectLabel>
+              {languages.map((supportedLang, index) => (
+                <SelectItem key={index} value={supportedLang}>
+                  {supportedLang}
+                </SelectItem>
+              ))}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </SettingsBody>
+    </SettingsItem>
+  );
+}
+
+export default LangSettings;
